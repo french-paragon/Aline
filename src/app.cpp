82e02ef8e967784db2236009294a7287cbf47b56
french-paragon <french.paragon@gmail.com>
2017-07-21 16:40:02 +0200

    Very simple implementation of the app class.

--- src/app.cpp
+++ src/app.cpp
@@ -1,1 +1,16 @@
 #include "app.h"
+
+namespace Aline {
+
+App::App(int argc, char **argv) :
+	QApplication(argc, argv)
+{
+
+}
+
+bool App::start(QString appCode) {
+	_appCode = appCode;
+	return true;
+}
+
+} // namespace Aline
--- src/app.h
+++ src/app.h
@@ -1,1 +1,24 @@
+#ifndef APP_H
+#define APP_H
+
 #include "aline_global.h"
+
+#include <QApplication>
+
+namespace Aline {
+
+class ALINE_EXPORT App : public QApplication
+{
+public:
+	App(int argc, char** argv);
+
+	virtual bool start(QString appCode = "default");
+
+protected:
+
+	QString _appCode;
+};
+
+} // namespace Aline
+
+#endif // APP_H
