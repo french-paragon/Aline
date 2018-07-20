0f69e86670be788bfb2ad99bc90139083f84b0c4
french-paragon <french.paragon@gmail.com>
2018-07-20 11:56:34 +0200

    Move a few json utils in Aline for reusability.

---
+++ src/utils/jsonutils.cpp
@@ -0,0 +1,411 @@
+#include "jsonutils.h"
+
+#include "model/editableitem.h"
+#include "model/labels/labelstree.h"
+#include "model/labels/label.h"
+#include "model/editableitemfactory.h"
+
+#include <QJsonArray>
+#include <QColor>
+#include <QPointF>
+#include <QSizeF>
+#include <QMetaObject>
+#include <QMetaProperty>
+
+
+const QString Aline::JsonUtils::LABEL_REF = "labels";
+
+const QString Aline::JsonUtils::LABEL_REF_ID = "reference";
+const QString Aline::JsonUtils::LABEL_NAME_ID = "name";
+const QString Aline::JsonUtils::LABEL_ITEMS_REFS_ID = "marked_items";
+const QString Aline::JsonUtils::LABEL_SUBLABELS_ID = "sublabels";
+
+const QString Aline::JsonUtils::ITEM_SUBITEM_ID = "item_internalsubitems";
+
+
+void Aline::JsonUtils::extractItemData(Aline::EditableItem* item, QJsonObject const& obj, EditableItemFactoryManager* subItemFactory, QStringList const& specialSkippedProperties, bool blockSignals) {
+
+	if (blockSignals) {
+		item->blockSignals(true);
+	}
+
+	for (QString prop : obj.keys()) {
+
+		if (prop == Aline::EditableItem::TYPE_ID_NAME) {
+			continue;
+		}
+
+		if (prop == Aline::EditableItem::CHILDREN_PROP_NAME) {
+			continue;
+		}
+
+		if (prop == Aline::EditableItem::REF_PROP_NAME) {
+			continue;
+		}
+
+		if (prop == ITEM_SUBITEM_ID) {
+			continue;
+		}
+
+		if (specialSkippedProperties.contains(prop)) {
+			continue;
+		}
+
+		const QMetaObject* meta = item->metaObject();
+		int prop_index = meta->indexOfProperty(prop.toStdString().c_str());
+		QVariant var = decodeVariantFromJson(obj.value(prop), meta->property(prop_index).type());
+
+		if (var.type() != meta->property(prop_index).type() && meta->property(prop_index).type() != QVariant::Invalid) {
+			var.convert(meta->property(prop_index).type());
+		}
+
+		item->setProperty(prop.toStdString().c_str(), var, true); //set all the properties.
+	}
+
+	if (obj.contains(ITEM_SUBITEM_ID)) {
+
+		QJsonValue subItems_val = obj.value(ITEM_SUBITEM_ID);
+
+		if (subItems_val.isArray()) {
+			QJsonArray arr = subItems_val.toArray();
+
+			for (QJsonValue v : arr) {
+
+				if (v.isObject()) {
+					QJsonObject subObj = v.toObject();
+
+					QString ref;
+					QString type;
+
+					if (subObj.contains(EditableItem::REF_PROP_NAME) &&
+							subObj.contains(EditableItem::TYPE_ID_NAME)) {
+
+						ref = subObj.value(EditableItem::REF_PROP_NAME).toString();
+						type = subObj.value(EditableItem::TYPE_ID_NAME).toString();
+
+						if (subItemFactory->hasSubItemFactoryInstalled(type)) {
+							Aline::EditableItem* subItem = subItemFactory->createSubItem(type, ref, item);
+
+							extractItemData(subItem, subObj, subItemFactory, specialSkippedProperties, blockSignals);
+
+							if (subItem != nullptr) {
+								item->insertSubItem(subItem);
+							}
+						}
+
+					}
+				}
+
+			}
+		}
+
+	}
+
+	if (blockSignals) {
+		item->blockSignals(false);
+	}
+
+}
+
+QJsonObject Aline::JsonUtils::encapsulateItemToJson(Aline::EditableItem* item) {
+
+	QJsonObject obj;
+
+	const QMetaObject* mobj = item->metaObject();
+
+	for (int i = 0; i < mobj->propertyCount(); i++) {
+
+		if (!mobj->property(i).isStored(item)) {
+			continue;
+		}
+
+		const char* prop = mobj->property(i).name();
+		QString sprop(prop);
+
+		obj.insert(sprop, encodeVariantToJson(item->property(prop)) ); // insert the property.
+	}
+
+	QList<QByteArray> dynamicProperties = item->dynamicPropertyNames();
+
+	for (QByteArray cpropName : dynamicProperties) {
+
+		QString sprop(cpropName);
+
+		obj.insert(sprop, encodeVariantToJson(item->property(cpropName.constData())));
+
+	}
+
+	QJsonArray internal_items_array;
+
+	for (Aline::EditableItem* internal_item : item->getSubItems()) {
+		QJsonObject sub_obj = encapsulateItemToJson(internal_item);
+
+		internal_items_array.append(sub_obj);
+
+	}
+
+	if (internal_items_array.size() > 0) {
+		obj.insert(ITEM_SUBITEM_ID, internal_items_array);
+	}
+
+	return obj;
+
+}
+
+QJsonValue Aline::JsonUtils::encodeVariantToJson(QVariant var) {
+
+	if (var.type() == QVariant::PointF) {
+		QPointF data = var.toPointF();
+
+		QString rep(QString("%1,%2").arg(data.x()).arg(data.y()));
+
+		return QJsonValue(rep);
+
+	} else if (var.type() == QVariant::SizeF) {
+
+		QSizeF data = var.toSizeF();
+
+		QString rep(QString("%1,%2").arg(data.width()).arg(data.height()));
+
+		return QJsonValue(rep);
+
+	} else if (var.type() == QVariant::Color) {
+
+		QColor col = var.value<QColor>();
+
+		return QJsonValue(col.name(QColor::HexArgb));
+	}
+
+	return QJsonValue::fromVariant(var);
+
+}
+QVariant Aline::JsonUtils::decodeVariantFromJson(QJsonValue val, QVariant::Type type) {
+
+	if (type == QVariant::PointF) {
+
+		QString rep = val.toString();
+
+		QStringList reps = rep.split(",");
+
+		if (reps.size() != 2) {
+			return QVariant();
+		}
+
+		QPointF pt;
+
+		bool ok;
+		qreal x = QVariant(reps[0]).toReal(&ok);
+
+		if (!ok) {
+			return QVariant();
+		}
+
+		qreal y = QVariant(reps[1]).toReal(&ok);
+
+		if (!ok) {
+			return QVariant();
+		}
+
+		pt.setX(x);
+		pt.setY(y);
+
+		return QVariant(pt);
+
+	} else if (type == QVariant::SizeF) {
+
+		QString rep = val.toString();
+
+		QStringList reps = rep.split(",");
+
+		if (reps.size() != 2) {
+			return QVariant();
+		}
+
+		QSizeF s;
+
+		bool ok;
+		qreal w = QVariant(reps[0]).toReal(&ok);
+
+		if (!ok) {
+			return QVariant();
+		}
+
+		qreal h = QVariant(reps[1]).toReal(&ok);
+
+		if (!ok) {
+			return QVariant();
+		}
+
+		s.setWidth(w);
+		s.setHeight(h);
+
+		return QVariant(s);
+
+	} else if (type == QVariant::Color) {
+
+		QString colorName = val.toString();
+
+		QColor col(colorName);
+
+		return QVariant(col);
+	}
+
+	return val.toVariant();
+
+}
+
+QJsonObject Aline::JsonUtils::encodeLabelAsJson(LabelsTree const* tree, QModelIndex const& index) {
+
+	QJsonObject obj;
+
+	QVariant name = tree->data(index, Qt::DisplayRole);
+	QVariant ref = tree->data(index, Aline::LabelsTree::LabelRefRole);
+
+	obj.insert(LABEL_NAME_ID, QJsonValue::fromVariant(name));
+	obj.insert(LABEL_REF_ID, QJsonValue::fromVariant(ref));
+
+	QVariant items_refs = tree->data(index, Aline::LabelsTree::LabelItemsRefsRole);
+
+	if (items_refs != QVariant()) {
+		obj.insert(LABEL_ITEMS_REFS_ID, QJsonValue::fromVariant(items_refs));
+	}
+
+	if (tree->rowCount(index) > 0) {
+
+		QJsonArray subLabels;
+
+		for (int i = 0; i < tree->rowCount(index); i++) {
+			QJsonObject subobj = encodeLabelAsJson(tree, tree->index(i, 0, index));
+			subLabels.push_back(QJsonValue(subobj));
+		}
+
+		obj.insert(LABEL_SUBLABELS_ID, QJsonValue(subLabels));
+
+	}
+
+	return obj;
+}
+
+Aline::Label* Aline::JsonUtils::extractJsonLabel(QJsonValue const& val, Aline::LabelsTree* parent) {
+
+	Aline::Label* l = new Aline::Label(parent);
+
+	try {
+		extractJsonLabelDatas(val, l);
+	} catch (JsonUtilsException const& e) {
+		Q_UNUSED(e);
+		delete l;
+		return nullptr;
+	}
+
+	return l;
+
+}
+Aline::Label* Aline::JsonUtils::extractJsonLabel(QJsonValue const& val, Aline::Label* parent) {
+
+	Aline::Label* l = new Aline::Label(parent);
+
+	try {
+		extractJsonLabelDatas(val, l);
+	} catch (JsonUtilsException const& e) {
+		Q_UNUSED(e);
+		delete l;
+		return nullptr;
+	}
+
+	return l;
+
+}
+void Aline::JsonUtils::extractJsonLabelDatas(QJsonValue const& val, Aline::Label* label) {
+
+	if (!val.isObject()) {
+		throw JsonUtilsException(LABEL_REF, "Error while extracting labels");
+	}
+
+	QJsonObject obj = val.toObject();
+
+	if (!obj.contains(LABEL_REF_ID) || !obj.contains(LABEL_NAME_ID)) {
+		throw JsonUtilsException(LABEL_REF, "Missing label name or reference.");
+	}
+
+	QJsonValue ref = obj.value(LABEL_REF_ID);
+	QJsonValue name = obj.value(LABEL_NAME_ID);
+
+
+	if (!ref.isString() || !name.isString()) {
+		throw JsonUtilsException(LABEL_REF, "Label name or reference can't be converted to string.");
+	}
+
+	label->setRef(ref.toString());
+	label->setObjectName(name.toString());
+
+	if (obj.contains(LABEL_ITEMS_REFS_ID)) {
+		QJsonValue items = obj.value(LABEL_ITEMS_REFS_ID);
+
+		if (items.isArray()) {
+			for (QJsonValue value : items.toArray()) {
+
+				if (value.isString()) {
+					label->markItem(value.toString());
+				}
+
+			}
+		}
+	}
+
+	if (obj.contains(LABEL_SUBLABELS_ID)) {
+		QJsonValue sublabels = obj.value(LABEL_SUBLABELS_ID);
+
+		if (sublabels.isArray()) {
+
+			for (QJsonValue val : sublabels.toArray()) {
+				Aline::Label* sublabel = extractJsonLabel(val, label);
+
+				if (sublabel != nullptr) {
+					label->addChild(sublabel);
+				}
+			}
+
+		}
+	}
+
+}
+
+
+Aline::JsonUtils::JsonUtilsException::JsonUtilsException (QString ref, QString infos) :
+	_ref(ref),
+	_infos(infos)
+{
+	QString what = "[Error while parsing \"" + _ref + "\"]: " + _infos;
+	_what = what.toStdString();
+}
+Aline::JsonUtils::JsonUtilsException::JsonUtilsException (JsonUtilsException const& other) :
+	JsonUtilsException(other.ref(), other.infos())
+{
+
+}
+
+const char* Aline::JsonUtils::JsonUtilsException::what() const throw() {
+
+	return _what.c_str();
+
+}
+
+
+QString Aline::JsonUtils::JsonUtilsException::ref() const
+{
+	return _ref;
+}
+
+QString Aline::JsonUtils::JsonUtilsException::infos() const
+{
+	return _infos;
+}
+
+void Aline::JsonUtils::JsonUtilsException::raise() const {
+	throw *this;
+}
+
+Aline::JsonUtils::JsonUtilsException * Aline::JsonUtils::JsonUtilsException::clone() const {
+	return new JsonUtilsException(*this);
+}
---
+++ src/utils/jsonutils.h
@@ -0,0 +1,69 @@
+#ifndef JSONUTILS_H
+#define JSONUTILS_H
+
+#include "../aline_global.h"
+
+#include <QVariant>
+#include <QJsonObject>
+#include <QJsonValue>
+#include <QException>
+
+class QModelIndex;
+
+namespace Aline {
+
+class EditableItem;
+class LabelsTree;
+class Label;
+class EditableItemFactoryManager;
+
+namespace JsonUtils {
+
+extern const QString LABEL_REF;
+
+extern const QString LABEL_REF_ID;
+extern const QString LABEL_NAME_ID;
+extern const QString LABEL_ITEMS_REFS_ID;
+extern const QString LABEL_SUBLABELS_ID;
+
+extern const QString ITEM_SUBITEM_ID;
+
+class ALINE_EXPORT JsonUtilsException : public QException
+{
+public:
+
+	JsonUtilsException (QString ref, QString infos);
+	JsonUtilsException (JsonUtilsException const& other);
+
+	const char* what() const throw();
+
+	void raise() const;
+	JsonUtilsException *clone() const;
+
+	QString ref() const;
+	QString infos() const;
+
+protected:
+
+	QString _ref;
+	QString _infos;
+	std::string _what;
+};
+
+ALINE_EXPORT void extractItemData(Aline::EditableItem* item, QJsonObject const& obj, EditableItemFactoryManager* subItemFactory, QStringList const& specialSkippedProperties = {}, bool blockSignals = true);
+ALINE_EXPORT QJsonObject encapsulateItemToJson(Aline::EditableItem* item);
+
+ALINE_EXPORT QJsonValue encodeVariantToJson(QVariant var);
+ALINE_EXPORT QVariant decodeVariantFromJson(QJsonValue val, QVariant::Type type);
+
+ALINE_EXPORT QJsonObject encodeLabelAsJson(LabelsTree const* tree, QModelIndex const& index);
+
+ALINE_EXPORT Aline::Label* extractJsonLabel(QJsonValue const& val, Aline::LabelsTree* parent);
+ALINE_EXPORT Aline::Label* extractJsonLabel(QJsonValue const& val, Aline::Label* parent);
+ALINE_EXPORT void extractJsonLabelDatas(QJsonValue const& val, Aline::Label* label);
+
+}
+
+} // namespace Aline
+
+#endif // JSONUTILS_H
