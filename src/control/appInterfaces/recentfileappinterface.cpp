c542d6705bd4900d3db88e7be4b5ee2f7f1a98f0
french-paragon <french.paragon@gmail.com>
2024-01-04 09:44:03 +0100

    Add an interface to manage recent files

---
+++ src/control/appInterfaces/recentfileappinterface.cpp
@@ -0,0 +1,133 @@
+#include "recentfileappinterface.h"
+
+#include <QSettings>
+#include <QFileInfo>
+
+namespace Aline {
+
+const QString RecentFileAppInterface::RecentFileInterfaceCode = "Aline_AppInterfaces_RecentFiles";
+const int RecentFileAppInterface::defaultRecentFilesSaved = 6;
+
+const QString RecentFileAppInterface::RecentFileListSetting = "Aline/RecentFiles/Paths";
+const QString RecentFileAppInterface::RecentFileNumberSetting = "Aline/RecentFiles/NFiles";
+
+RecentFileAppInterface::RecentFileAppInterface(QObject *parent)
+	: QObject{parent}
+{
+
+}
+
+QStringList RecentFileAppInterface::getRecentFilesPaths() const {
+
+	QSettings settings;
+
+	if (!settings.contains(RecentFileListSetting)) {
+		return QStringList();
+	}
+
+	QStringList lst = settings.value(RecentFileListSetting).toStringList();
+
+	QStringList ret;
+	ret.reserve(lst.size());
+
+	for (QString const& path : lst) {
+		QFileInfo info(path);
+
+		if (!info.exists()) {
+			continue;
+		}
+
+		ret.push_back(path);
+	}
+
+	return ret;
+
+}
+int RecentFileAppInterface::getNRecentFiles() const {
+
+	QSettings settings;
+
+	if (!settings.contains(RecentFileNumberSetting)) {
+		return defaultRecentFilesSaved;
+	}
+
+	bool ok = true;
+	int val = settings.value(RecentFileNumberSetting).toInt(&ok);
+
+	if (!ok) {
+		settings.setValue(RecentFileNumberSetting, defaultRecentFilesSaved);
+		return defaultRecentFilesSaved;
+	}
+
+	return val;
+
+}
+
+void RecentFileAppInterface::addNewRecentFile(QString filePath) {
+
+	QFileInfo infos(filePath);
+
+	QSettings settings;
+
+	QStringList lst = settings.value(RecentFileListSetting).toStringList();
+	int maxSize = getNRecentFiles();
+
+	QStringList modified;
+	modified.reserve(std::max(lst.size()+1, maxSize));
+	modified.push_back(infos.canonicalFilePath());
+
+	for (QString const& path : lst) {
+		QFileInfo prevInfos(path);
+
+		if (!prevInfos.exists()) {
+			continue;
+		}
+
+		if (prevInfos.canonicalFilePath() == infos.canonicalFilePath()) {
+			continue;
+		}
+
+		modified.push_back(path);
+	}
+
+	settings.setValue(RecentFileListSetting, modified);
+
+}
+
+void RecentFileAppInterface::setNumberOfRecentFiles(int num) {
+
+	if (num <= 0) {
+		return;
+	}
+
+	QSettings settings;
+	settings.setValue(RecentFileNumberSetting, num);
+
+	if (!settings.contains(RecentFileListSetting)) {
+		return;
+	}
+
+	QStringList lst = settings.value(RecentFileListSetting).toStringList();
+
+	QStringList modified;
+	modified.reserve(lst.size());
+
+	for (QString const& path : lst) {
+		QFileInfo info(path);
+
+		if (!info.exists()) {
+			continue;
+		}
+
+		modified.push_back(path);
+	}
+
+	if (modified.size() > num) {
+		modified = modified.mid(0, num);
+	}
+
+	settings.setValue(RecentFileListSetting, modified);
+
+}
+
+} // namespace Aline
---
+++ src/control/appInterfaces/recentfileappinterface.h
@@ -0,0 +1,39 @@
+#ifndef ALINE_RECENTFILEAPPINTERFACE_H
+#define ALINE_RECENTFILEAPPINTERFACE_H
+
+#include <QObject>
+
+namespace Aline {
+
+/*!
+ * \brief The RecentFileAppInterface class provide an interface to manage a list of recent files
+ *
+ * This interface store a persistent list of recent files. It checks that files exists when adding them and as well as before returning the list.
+ *
+ * The storage mechanism rely on QSettings, so the organisation name and dommain need to be configured in the application.
+ */
+class RecentFileAppInterface : public QObject
+{
+	Q_OBJECT
+public:
+	static const QString RecentFileInterfaceCode;
+	static const int defaultRecentFilesSaved;
+
+	static const QString RecentFileListSetting;
+	static const QString RecentFileNumberSetting;
+
+	explicit RecentFileAppInterface(QObject *parent = nullptr);
+
+	QStringList getRecentFilesPaths() const;
+	int getNRecentFiles() const;
+
+	void addNewRecentFile(QString filePath);
+	void setNumberOfRecentFiles(int num);
+
+Q_SIGNALS:
+
+};
+
+} // namespace Aline
+
+#endif // ALINE_RECENTFILEAPPINTERFACE_H
