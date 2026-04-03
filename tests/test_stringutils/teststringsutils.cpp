7d2c0ce13cb35b7f80d2cf38f5cab3131d5404fc
french-paragon <french.paragon@gmail.com>
2026-04-03 18:36:16 +0200

    Improve remove accent function

---
+++ tests/test_stringutils/teststringsutils.cpp
@@ -0,0 +1,30 @@
+#include <QTest>
+
+#include "Aline/utils/stringutils.h"
+
+
+class TestStringUtils : public QObject {
+
+    Q_OBJECT
+private Q_SLOTS:
+
+    void initTestCase();
+    void testRemoveAccent();
+
+private:
+
+};
+
+void TestStringUtils::initTestCase() {
+
+}
+void TestStringUtils::testRemoveAccent() {
+
+    QCOMPARE(Aline::StringUtils::removeAccents("L'heure du thé à l'échêché!"),"L'heure du the a l'echeche!");
+    QCOMPARE(Aline::StringUtils::removeAccents("Échasse à l'Aïkido"),"Echasse a l'Aikido");
+    QCOMPARE(Aline::StringUtils::removeAccents("ÀàÄäÈèÉéÖöÜüÂâÎî"),"AaAaEeEeOoUuAaIi");
+
+}
+
+QTEST_MAIN(TestStringUtils)
+#include "teststringsutils.moc"
